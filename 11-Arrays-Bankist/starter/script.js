8d9937a480b0773988eaf4b9dda43072aa09b18c
mganapatvamsi1 <ganapathiraju.vamsi@gmail.com>
2022-01-31 00:50:32 -0800

    removing unused

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -66,6 +66,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
+
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
@@ -77,7 +78,77 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
-let arr = ['a', 'b', 'c', 'd', 'e'];
-function iterate(item) { console.log(item); }
-arr.forEach(iterate);
-console.log(arr.slice(1));
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// function iterate(item) { console.log(item); }
+// arr.forEach(iterate);
+// console.log(arr.slice(1));
+
+// Async - await
+// make a function to handle error
+function handleError(fun) {
+  return function (...params) {
+    return fun(...params).catch(function (err) {
+      console.error(`Oops`, err);
+    });
+  };
+}
+// re-writing the same above function using arrow functions
+const handleError2 =
+  fun =>
+  (...params) =>
+    // fun(...params).catch(err => console.error(`Oops again!`, err));
+    fun(...params).catch(console.error);
+
+const catchErrors1 = fn => {
+  return function (req, res, next) {
+    return fn(req, res, next).catch(next);
+  };
+};
+
+//hot shot
+const catchErrors2 = fn => (req, res, next) => fn(req, res, next).catch(next);
+
+console.log(`person1: shows ticket`);
+console.log(`person2: shows ticket`);
+
+const promiseMyWifeBringTicket = new Promise((resolve, reject) => {
+  setTimeout(() => resolve(`ticket`), 4000);
+});
+
+// promiseMyWifeBringTicket
+//   .then(t => console.log(`person3: shows ${t}.`))
+//   .catch(console.error);
+// .catch(err => console.error(`oops error occurred: ${err}`));
+
+const getPopCorn = promiseMyWifeBringTicket
+  .then(t => {
+    console.log(`------START OF POPCORN------`);
+    console.log(`wife: I got the tickets`);
+    console.log(`hus: we should walk in`);
+    console.log(`wife: I am hungry so need some popcorn`);
+    console.log(`------END OF POPCORN------`);
+    return new Promise((resolve, reject) =>
+      resolve(`we got ${t} and popcorn now.`)
+    );
+  })
+  .catch(console.error);
+
+const getButterAndDrink = getPopCorn
+  .then(t => {
+    console.log(`------START OF BUTTER AND DRINK------`);
+    console.log(`hus: Popcorn's ready`);
+    console.log(`hus: now shall we go inside`);
+    console.log(`wife: I want some butter on my popcorn and a soda`);
+    console.log(`------END OF BUTTER AND DRINK------`);
+    return new Promise((resolve, reject) =>
+      resolve(
+        `ok wifu.. now we have ticket, popcorn with butter added and coke too.`
+      )
+    );
+  })
+  .catch(console.error);
+
+getButterAndDrink.then(console.log).catch(console.error);
+
+console.log(`person4: shows ticket`);
+console.log(`person5: shows ticket`);

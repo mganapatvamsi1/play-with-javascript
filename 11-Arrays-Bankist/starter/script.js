e983bd68d567baeb9a85703adcf247a3a4a9a766
Manikanta Ganapathiraju <mganapathiraju@expediagroup.com>
2022-03-03 12:21:39 -0800

    array methods 1

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -124,3 +124,5 @@ console.log('ManiG'.at(-1)); // G
 console.log('ManiG'.at(0)); // M
 
 // Looping Arrays: forEach
+const testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+testArr.forEach(ele => console.log(ele));

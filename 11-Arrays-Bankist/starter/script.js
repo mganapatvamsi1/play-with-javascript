8f0f334f83c2fe12e723f365c42a2a172eefae3d
mganapatvamsi1 <ganapathiraju.vamsi@gmail.com>
2022-06-20 07:02:38 -0700

    JS practice

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -2,77 +2,15 @@
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// BANKIST APP
-
 /*
-// Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
-
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
-
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
-
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
-
-const accounts = [account1, account2, account3, account4];
-
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
-
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
-
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
-
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
-*/
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // LECTURES
-
 // const currencies = new Map([
 //   ['USD', 'United States dollar'],
 //   ['EUR', 'Euro'],
 //   ['GBP', 'Pound sterling'],
 // ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
 
 /////////////////////////////////////////////////
 let arr = ['a', 'b', 'c', 'd', 'e'];
@@ -201,3 +139,94 @@ items.forEach((ele) => copyItemsNew.push(ele))
 console.log(`updated state of copyItemsNew using forEach:: `, copyItemsNew)
 
 console.log('-----END OF FOREACH-----');
+*/
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+// Data
+const account1 = {
+  owner: 'Manikanta Ganapathiraju',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Tuyen Le',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Raj Aryan',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Rahul Malhotra',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+// const labelWelcome = document.querySelector('.welcome');
+// const labelDate = document.querySelector('.date');
+// const labelBalance = document.querySelector('.balance__value');
+// const labelSumIn = document.querySelector('.summary__value--in');
+// const labelSumOut = document.querySelector('.summary__value--out');
+// const labelSumInterest = document.querySelector('.summary__value--interest');
+// const labelTimer = document.querySelector('.timer');
+
+// const containerApp = document.querySelector('.app');
+// const containerMovements = document.querySelector('.movements');
+
+// const btnLogin = document.querySelector('.login__btn');
+// const btnTransfer = document.querySelector('.form__btn--transfer');
+// const btnLoan = document.querySelector('.form__btn--loan');
+// const btnClose = document.querySelector('.form__btn--close');
+// const btnSort = document.querySelector('.btn--sort');
+
+// const inputLoginUsername = document.querySelector('.login__input--user');
+// const inputLoginPin = document.querySelector('.login__input--pin');
+// const inputTransferTo = document.querySelector('.form__input--to');
+// const inputTransferAmount = document.querySelector('.form__input--amount');
+// const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+// const inputCloseUsername = document.querySelector('.form__input--user');
+// const inputClosePin = document.querySelector('.form__input--pin');
+
+// const displayMovements = function(movements) {
+//   containerMovements.innerHTML = '';
+
+//   movements.forEach(function (mov, index) {
+//     const transactionType = mov > 0 ? 'deposit' : 'withdrawal';
+
+//     const html = `
+//     <div class="movements__row">
+//       <div class="movements__type movements__type--${transactionType}">${index + 1} ${transactionType}</div>
+//       <div class="movements__value">${mov}€</div>
+//     </div>
+//     `;
+    
+//     // need to attach this 'html' into the current container::
+//     containerMovements.insertAdjacentHTML('afterbegin', html);
+//     // containerMovements.insertAdjacentHTML('beforeend', html);
+//   });
+
+// }
+// displayMovements(account1.movements)
+// console.log(containerMovements.innerHTML)
+
+// data transformations (map, filter and reduce)
+const testArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const newArr1 = testArr.map(ele => ele * 2);
+const newArr2 = testArr
+                    .filter(ele => ele % 2 !== 0)
+                    .map(ele => ele * 3);
+console.log('newArr1:: ', newArr1);
+console.log('newArr2:: ', newArr2);
